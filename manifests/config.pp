bf9fa197bd9112a9802a057342be640f364c78d3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-19 10:59:55 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,8 +4,8 @@ class windows_base::config {
 
 # Make sure hosts file is ok. Did not use fqdn, if used Oracle gives error message: PRKN-1019.
   host { $::hostname:
-    ensure       => 'present',
-    ip           => $::ipaddress,
+    ensure => 'present',
+    ip     => $::ipaddress,
   }
 
   host { 'localhost':
